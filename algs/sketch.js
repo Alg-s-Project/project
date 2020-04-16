40a9754eddd6a89c64395376f3287c0706d09c4c
Christian Moronta <cmoronta26@gmail.com>
2020-04-16 17:08:43 -0400

    Add documentation to code

--- algs/sketch.js
+++ algs/sketch.js
@@ -12,18 +12,19 @@ function setup() {
   circleNum = createInput(numSlider.value().toString(), "number");
   circleNum.addClass("input");
 
-  // change value of input to slider value
+  // change value of # of circles input to slider value
   numSlider.input(sliderChange);
 
+  // create diameter slider
   dSlider = createSlider(10, 30, 20);
   dSlider.addClass('slider')
   dSlider.input(dsliderChange)
 
-  // create diameter
+  // create diameter input box
   diameter = createInput("20", "number");
   diameter.addClass("input");
 
-  // create button
+  // create populate button
   let button = createButton("Populate");
   button.style("margin", "12px");
   button.mousePressed(buttonPressed);
@@ -37,24 +38,31 @@ function dsliderChange() {
   diameter.value(dSlider.value())
 }
 
+// Clears canvas and repopulates with circles with input values from above
 function buttonPressed() {
+  // loop() runs the draw function once
   loop();
+  // boolean to hold value of whether loop has been called. Used so that draw is only run once button pressed and not at page load
   isNoLoop = false;
   clear();
+  // clears canvas to repopulate
   createCanvas(900,600)
   background(15,0,146)
 }
 
+// Sets value of number of circles input box to its slider's value 
 function sliderChange() {
   circleNum.value(numSlider.value());
 }
 
 function draw() {
+  // if-else to check whether to run or not. uses the isNoLoop variable to check whether draw should run. 
   if (isNoLoop) {
     return;
   } else {
     noLoop();
     isNoLoop = true;
+    // Randomly populate canvas with circles depending on inputted values
     for (let i = 0; i < circleNum.value(); i++) {
       circle(randomCoord(900), randomCoord(600), diameter.value());
     }
